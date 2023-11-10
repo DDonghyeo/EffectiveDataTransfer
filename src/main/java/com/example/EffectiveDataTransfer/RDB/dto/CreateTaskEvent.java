0d23e0b9b047de3d50b215d31e57e2d613114364
DDonghyeo <gsu9045000@gmail.com>
2023-11-10 21:21:47 +0900

    [RabbitMQ] Dead Letter Queue

--- src/main/java/com/example/EffectiveDataTransfer/RDB/dto/CreateTaskEvent.java
+++ src/main/java/com/example/EffectiveDataTransfer/RDB/dto/CreateTaskEvent.java
@@ -2,12 +2,13 @@ package com.example.EffectiveDataTransfer.RDB.dto;
 
 import com.example.EffectiveDataTransfer.RDB.Entity.Event;
 import com.example.EffectiveDataTransfer.RDB.Entity.Task;
+import com.example.EffectiveDataTransfer.RDB.Enum.EventStatus;
 
 public class CreateTaskEvent {
 
     public static Event of(Task task) {
         return Event.builder()
-                .status(0)
+                .status(EventStatus.READY)
                 .payload(task.getTask_content())
                 .build();
     }
